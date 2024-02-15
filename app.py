7d532b4c4ff2734dbf971b127d3ac547ac7709f8
Gareki1 <118125437+Gareki1@users.noreply.github.com>
2024-02-15 15:50:59 +0000

    Improvements

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
 from flask import Flask, render_template, jsonify
-from database import load_authors_from_db
+from database import load_authors_from_db, load_author_details
 
 app = Flask(__name__)
-
-# AUTHORS = [{
-#     'id': 1,
-#     'authorname': 'Charles Dickens',
-#     'bookname': 'David Copperfield',
-#     'review': 'Kino.'
-# }, {
-#     'id': 2,
-#     'authorname': 'Richard Bach',
-#     'bookname': 'Jonathan Livingston Seagull',
-#     'review': 'Incredible.'
-# }]
-
 
 @app.route("/")
 def home():
@@ -27,6 +14,14 @@ def list():
   api_authors = load_authors_from_db()
   return jsonify(api_authors)
 
+@app.route("/author/<id>")
+def details(id):
+  author = load_author_details(id)
+  if not author:
+    return "Not Found", 404
+    
+  return render_template('author.html', author=author)
+  #return jsonify(author)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -7,8 +7,17 @@ engine = create_engine(DB_STRING)
 
 def load_authors_from_db():
   with engine.connect() as conn:
-    result = conn.execute(text("select authorname, bookname, review from authors"))
+    result = conn.execute(text("select * from authors"))
     authors = []
     for row in result.all():
       authors.append(row._asdict())
     return authors
+
+def load_author_details(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM authors WHERE id = :val"), {"val": id})
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return rows[0]._asdict()
